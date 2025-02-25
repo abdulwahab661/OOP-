5299c78a6e8cf0a838723a4598c837cb3cb57007
Abdul Wahab <wahab98298532@gmail.com>
2025-02-25 17:38:28 +0500

    Add files via upload

---
+++ UserInfo.java
@@ -0,0 +1,38 @@
+class UserInfo{
+
+ private static String Username="AbdulWahab123";
+ private static String Password="Abc123"; 
+
+ public String getUsername(){
+
+
+ return Username;
+
+   }
+
+ public String setUsername(String Username){
+
+  this.Username=Username;
+  return Username;
+ 
+
+   }
+
+ public String getPassword(){
+
+
+ return Password;
+
+   }
+
+ public String setPassword(String Password){
+
+ this.Password=Password;
+ return Password;
+ 
+
+   }
+
+
+
+}
