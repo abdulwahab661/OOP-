f038836884d74f2035bf5962d7e971fa5c1d9227
Abdul Wahab <wahab98298532@gmail.com>
2025-02-25 17:19:35 +0500

    Add files via upload

---
+++ StudentTest.java
@@ -0,0 +1,47 @@
+class StudentTest{
+
+   public static void main(String args[]){
+ 
+
+  Student obj; 
+  Student obj1;
+  obj = new Student("Abdul Wahab","Johar Town","Lahore",3.5);
+  obj1 = new Student("Shahmeer","DHA","Lahore",3.3);
+
+
+  
+  obj.displayInfo();
+  obj1.displayInfo();
+ 
+ 
+  obj.setName("Shahmeer");
+  obj.displayInfo();
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+ }
+
+
+
+
